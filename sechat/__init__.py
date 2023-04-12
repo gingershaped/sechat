7160ac1d25476ec044b97edd288f9c465baf28c2
Ginger <75683114+gingerindustries@users.noreply.github.com>
2023-04-12 12:35:34 +0000

    add some thigns to __all__

--- sechat/__init__.py
+++ sechat/__init__.py
@@ -1,6 +1,6 @@
 from sechat.bot import Bot
 from sechat.room import Room
 from sechat.version import __version__
-from sechat.events import Event, EventType, MentionEvent, MessageEvent
+from sechat.events import Event, EventType, MentionEvent, MessageEvent, UnknownEvent, EditEvent
 
-__all__ = ["Bot", "Room", "Event", "EventType", "MentionEvent", "MessageEvent"]
+__all__ = ["Bot", "Room", "Event", "EventType", "MentionEvent", "MessageEvent", "UnknownEvent", "EditEvent"]
