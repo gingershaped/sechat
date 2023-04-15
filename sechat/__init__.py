960505afea5fa6310dd1121c30ea135f78b5eaee
Ginger <75683114+gingerindustries@users.noreply.github.com>
2023-04-15 21:26:27 +0000

    as wel as that

--- sechat/__init__.py
+++ sechat/__init__.py
@@ -1,6 +1,6 @@
 from sechat.bot import Bot
 from sechat.room import Room
 from sechat.version import __version__
-from sechat.events import Event, EventType, MentionEvent, MessageEvent, UnknownEvent, EditEvent
+from sechat.events import Event, EventType, MentionEvent, MessageEvent, UnknownEvent, EditEvent, ReplyEvent
 
-__all__ = ["Bot", "Room", "Event", "EventType", "MentionEvent", "MessageEvent", "UnknownEvent", "EditEvent"]
+__all__ = ["Bot", "Room", "Event", "EventType", "MentionEvent", "MessageEvent", "UnknownEvent", "EditEvent", "ReplyEvent"]
