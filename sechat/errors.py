5859bd48bcbeab30184bb09b4d49751b5f23053e
Ginger <75683114+gingershaped@users.noreply.github.com>
2024-10-23 15:11:03 +0200

    Reword error docstrings

--- sechat/errors.py
+++ sechat/errors.py
@@ -1,14 +1,20 @@
+from typing import Any
+
+
 class ChatException(Exception):
     """Base exception type"""
     pass
 
 class LoginError(ChatException):
-    """Used for errors while logging in"""
+    """Raised when an error occurs while logging in"""
 
     pass
 
 class RatelimitError(ChatException):
-    """Used when a ratelimit is hit"""
+    """Raised when a ratelimit is hit.
+    
+    This error is automatically handled by the library and should never escape to user code.
+    """
 
     def __init__(self, retry_after: int):
         super().__init__()
@@ -16,5 +22,5 @@ class RatelimitError(ChatException):
 
 
 class OperationFailedError(ChatException):
-    """Used when an operation fails"""
+    """Raised when chat returns an unexpected response"""
     pass
