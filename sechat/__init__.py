b63d84e89cd1534af40ada61f8167bc068dd069a
Ginger <75683114+gingershaped@users.noreply.github.com>
2025-01-05 15:23:03 -0500

    Add Profile to __all__

--- sechat/__init__.py
+++ sechat/__init__.py
@@ -2,8 +2,9 @@ from importlib.metadata import version
 
 from sechat.credentials import Credentials
 from sechat.room import Room
+from sechat.profile import Profile
 from sechat.servers import Server
 from sechat import events, errors
 
 __version__ = version(__name__)
-__all__ = ["Credentials", "Room", "Server", "events", "errors"]
+__all__ = ["Credentials", "Room", "Profile", "Server", "events", "errors"]
