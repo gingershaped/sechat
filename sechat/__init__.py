1b910f2d7fb51a84e046acab0617085ba87da285
Ginger <75683114+gingershaped@users.noreply.github.com>
2025-03-14 13:31:59 -0400

    Move Room.upload_image to ChatClient

--- sechat/__init__.py
+++ sechat/__init__.py
@@ -2,9 +2,10 @@ from importlib.metadata import version
 
 from sechat.credentials import Credentials
 from sechat.room import Room
+from sechat.client import ChatClient
 from sechat.profile import Profile
 from sechat.servers import Server
 from sechat import events, errors
 
 __version__ = version(__name__)
-__all__ = ["Credentials", "Room", "Profile", "Server", "events", "errors"]
+__all__ = ["Credentials", "Room", "ChatClient", "Profile", "Server", "events", "errors"]
--- sechat/room.py
+++ sechat/room.py
@@ -332,30 +332,3 @@ class Room(ChatClient):
             slug: The slug of the conversation to delete.
         """
         await self._ok_request(f"/conversation/delete/{self.room_id}/{slug}")
-
-    async def upload_image(self, image: BytesIO, filename: str = "upload") -> str:
-        """Upload an image to Stack Exchange's image hosting.
-        
-        Parameters:
-            image: The image data to upload.
-            filename: The filename supplied to Stack Exchange.
-                It is unknown if this parameter is used by anything.
-        Returns:
-            The URL of the uploaded image.
-        """
-
-        response = await self._session.post("/upload/image", data={"filename": image, "shadow-filename": filename})
-        soup = BeautifulSoup(await response.read(), features="lxml")
-        assert isinstance(script := soup.find("script"), Tag)
-        
-        try:
-            error_line, result_line, *_ = map(str.strip, script.get_text().strip().splitlines())
-        except ValueError:
-            raise OperationFailedError("Malformed response", await response.text()) from None
-        error = re.match(r"var error = (?:'(.*)'|null);", error_line)
-        result = re.match(r"var result = '(.*)';", result_line)
-        if error is None or result is None:
-            raise OperationFailedError("Malformed response", await response.text())
-        if error.group(1) != None:
-            raise OperationFailedError(f"Upload failed: {error.group(1)}")
-        return cast(str, result.group(1))
